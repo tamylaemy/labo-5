148ab2cc848657cf2eb0c35fde7061cad074490b
Tamyla Ait-Chellouche <tamyla-emy@hotmail.fr>
2021-02-17 16:29:10 -0500

    Sass et php, cours 16 février

--- themes/theme-4w4/theme-4w4/footer.php
+++ themes/theme-4w4/theme-4w4/footer.php
@@ -13,17 +13,19 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'theme-4w4' ) ); ?>">
+			<!-- a href="<?php //echo esc_url( __( 'https://wordpress.org/', 'Collège de Maisonneuve' ) ); ?>" -->
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'theme-4w4' ), 'WordPress' );
+				// printf( esc_html__( 'Proudly powered by %s', 'theme-4w4' ), 'WordPress' );
 				?>
 			</a>
 			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'theme-4w4' ), 'theme-4w4', '<a href="http://underscores.me/">Tamyla Ait-Chellouche</a>' );
+				// printf( esc_html__( 'Theme: %1$s by %2$s.', 'theme-4w4' ), 'theme-4w4', '<a href="http://underscores.me/">Tamyla Ait-Chellouche</a>' );
 				?>
+
+			<a href='https://cmaisonneuve.qc.ca>'>Collège de Maisonneuve</a>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
--- themes/theme-4w4/theme-4w4/functions.php
+++ themes/theme-4w4/theme-4w4/functions.php
@@ -141,8 +141,9 @@ add_action( 'widgets_init', 'theme_4w4_widgets_init' );
  */
 function theme_4w4_scripts() {
 	wp_enqueue_style( 'theme-4w4-style', get_stylesheet_uri(), array(), _S_VERSION );
+	wp_enqueue_style( 'theme-4w4-google-fonts', 'https://fonts.googleapis.com/css2?family=Montserrat:wght@300;600&display=swap', false ); 
 	wp_style_add_data( 'theme-4w4-style', 'rtl', 'replace' );
-
+	
 	wp_enqueue_script( 'theme-4w4-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
