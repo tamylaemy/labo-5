6cd833c62ae10250ad91e4dd286ffade4dfdcbdb
Tamyla Ait-Chellouche <tamyla-emy@hotmail.fr>
2021-02-23 16:05:22 -0500

    Ajout function.php du carrousel

--- themes/theme-4w4/theme-4w4/functions.php
+++ themes/theme-4w4/theme-4w4/functions.php
@@ -145,7 +145,14 @@ function theme_4w4_scripts() {
 	wp_enqueue_style( 'theme-4w4-google-fonts', 'https://fonts.googleapis.com/css2?family=Montserrat:wght@300;600&display=swap', false ); 
 	
 	wp_style_add_data( 'theme-4w4-style', 'rtl', 'replace' );
+
 	wp_enqueue_script( 'theme-4w4-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+
+	wp_register_script( 'theme-4w4-carrousel' , get_template_directory_uri() . '/js/carrousel.js', array(), _S_VERSION, true );
+
+	if ( is_front_page()) {
+		wp_enqueue_script( 'theme-4w4-carrousel' );
+	}
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
